e6d9bd3aceda72bbb5541898e51744cd6d07c521
david-s99 <57419969+david-s99@users.noreply.github.com>
2020-09-08 16:40:12 -0600

    Update euler.py

--- euler.py
+++ euler.py
@@ -1,6 +1,17 @@
 def e_cuadratica(n):
-    #Imple
-    return 0
+    i=0
+    res=0
+    while (i<=n):
+        res+= 1/(factorial(i))
+        i+=1
+    return res
+
+def factorial(n):
+    res=1
+    while(n>0):
+        res=res*n
+        n=n-1
+    return res
 
 
 def e_lineal(n):
