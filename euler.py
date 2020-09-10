a5754697bff3f146ea9ad612d69d9880dfe76c86
David Solano <davidsolano121@yahoo.com>
2020-09-09 19:37:44 -0600

    Tarea Corta 1, David Solano

--- euler.py
+++ euler.py
@@ -1,19 +1,23 @@
+import math
 def e_cuadratica(n):
     i=0
     res=0
     while (i<=n):
-        res+= 1/(factorial(i))
+        res+= 1/(fact(i))
         i+=1
     return res
 
-def factorial(n):
+def e_lineal(n):
+        i=0
+        res=0
+        while (i<=n):
+	    factorial= fact(i)
+            res+= 1/factorial
+            i+=1
+        return res
+def fact(n):
     res=1
     while(n>0):
         res=res*n
         n=n-1
     return res
-
-
-def e_lineal(n):
-    # Implemente esa función
-    return 0
